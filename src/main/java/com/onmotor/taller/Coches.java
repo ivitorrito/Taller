ecc475a362513d15229bf4373e4335d9f47b7b54
Taller <ivan@copiadorascostaluz.com>
2023-04-03 07:35:14 +0200

    chackbox

--- src/main/java/com/onmotor/taller/Coches.java
+++ src/main/java/com/onmotor/taller/Coches.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.onmotor.taller;
 
-/**
- *
- * @author ivan
- */
+
 public class Coches {
     String marca;
     String modelo;
